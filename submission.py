a36ef6bce20412d8cccc49abd952f42717afb8b3
haythamh98 <49315851+haythamh98@users.noreply.github.com>
2021-06-01 17:17:17 +0300

    part A almost done, need to delete extra code

--- submission.py
+++ submission.py
@@ -1,3 +1,5 @@
+import math
+
 import logic
 import random
 from AbstractPlayers import *
@@ -22,14 +24,16 @@ class GreedyMovePlayer(AbstractMovePlayer):
     the player receives time limit for a single step and the board as parameter and return the next move that gives
     the best score by looking one step ahead.
     """
+
     def get_move(self, board, time_limit) -> Move:
         optional_moves_score = {}
         for move in Move:
-            new_board, done, score = commands[move](board)
+            new_board, done, score = commands[move](board)  # do a run for the board, on trying the "move" direction
             if done:
                 optional_moves_score[move] = score
 
-        return max(optional_moves_score, key=optional_moves_score.get)
+        return max(optional_moves_score,
+                   key=optional_moves_score.get)  # return comparing to their best value (aka get function)
 
 
 class RandomIndexPlayer(AbstractIndexPlayer):
@@ -37,6 +41,7 @@ class RandomIndexPlayer(AbstractIndexPlayer):
     the player receives time limit for a single step and the board as parameter and return the next indices to
     put 2 randomly.
     """
+
     def get_indices(self, board, value, time_limit) -> (int, int):
         a = random.randint(0, len(board) - 1)
         b = random.randint(0, len(board) - 1)
@@ -46,21 +51,134 @@ class RandomIndexPlayer(AbstractIndexPlayer):
         return a, b
 
 
+# optimization: big numbers close to wall
 # part A
 class ImprovedGreedyMovePlayer(AbstractMovePlayer):
     """Improved greedy Move Player,
     implement get_move function with greedy move that looks only one step ahead with heuristic.
     (you can add helper functions as you want).
     """
+
     def __init__(self):
         AbstractMovePlayer.__init__(self)
         # TODO: add here if needed
 
     def get_move(self, board, time_limit) -> Move:
-        # TODO: erase the following line and implement this function.
-        raise NotImplementedError
+        optional_moves_score = {}
+        for move in Move:
+            new_board, done, score = commands[move](board)  # do a run for the board, on trying the "move" direction
+            if done:
+                random.seed()
+                # maximize score, while keep big numbers close to a wall, and prefer steps that increase the number of empty tiles
+                empty_tiles = max(self.get_number_of_empty_tiles(new_board), 1)
+                closely_score = self.get_closly_score(new_board)
+                sum_all = max(self.sum_all_tiles(new_board), 1)
+                corn_score = max(self.corner_score(new_board), 1)
+                close_to_wall_scor = max(self.close_to_wall_score(new_board), 1)
+                div_wi = 64
+                aa,bb, bigges = self.get_biggest_tile_pos(new_board)
+                AMP = math.log2(bigges)
+                hur = empty_tiles / 16 + 1*corn_score / (div_wi * sum_all) + (close_to_wall_scor / (div_wi * sum_all))
+                #print(f'empty_tiles {empty_tiles} corn_score = {corn_score} close_to_wall_scor = {close_to_wall_scor}  ')
+                optional_moves_score[move] =  score + AMP*hur
+                #print(f'score {score} hur = {hur} final = {optional_moves_score[move]}')
+        print(optional_moves_score[max(optional_moves_score,
+                   key=optional_moves_score.get)] )
+        return max(optional_moves_score,
+                   key=optional_moves_score.get)  # return comparing to their best value (aka get function)
 
-    # TODO: add here helper functions in class, if needed
+    def popMin(self, my_dict):
+        if len(my_dict) == 0:
+            return
+        mini = min(my_dict, key=my_dict.get)
+        for key, value in my_dict.items():
+            if value == mini:
+                my_dict.pop(key)
+
+    def close_to_wall_score(self, board) -> int:
+        to_ret = 0
+        for i in range(0, len(board)):
+            to_ret += board[i][0]
+            to_ret += board[i][-1]
+        for i in range(1, len(board) - 1):
+            to_ret += board[0][i]
+            to_ret += board[-1][i]
+        return to_ret
+
+    def get_closly_score(self, board) -> int: # return 1 if the biggest number has value = x, and next to it tile with x/2
+        i, j, val = self.get_biggest_tile_pos(board)
+        if i == -1:
+            return 0
+        if i-1 > 0:
+            if board[i-1][j] == val/2:
+                return 1
+        if j-1 > 0:
+            if board[i][j-1] == val/2:
+                return 1
+        if i + 1 < len(board):
+            if board[i + 1][j] == val/2:
+                return 1
+        if j + 1 < len(board):
+            if board[i][j+1] == val/2:
+                return 1
+        return 0
+
+    def sum_all_tiles(self, board) -> int:
+        to_ret = 0
+        for i in range(0, len(board)):
+            for j in range(0, len(board)):
+                to_ret += board[i][j]
+        return to_ret
+
+    def corner_score(self, board) -> int:
+        to_ret = 0
+        to_ret += board[-1][0]
+        to_ret += board[-1][-1]
+        to_ret += board[0][-1]
+        to_ret += board[0][0]
+        return to_ret
+
+    def get_number_of_empty_tiles(self, board) -> int:
+        to_ret = 0
+        for i in range(0, len(board)):
+            for j in range(0, len(board)):
+                if board[i][j] == 0:
+                    to_ret += 1
+        return to_ret
+
+    def get_biggest_new_merge(self, board, old) -> int:
+        to_ret = 0
+        for i in range(0, len(board)):
+            for j in range(0, len(board)):
+                if board[i][j] == 0:
+                    to_ret += 1
+        return to_ret
+
+    def get_number_of_tiles_attached_to_wall(self, board) -> int:
+        to_ret = 0
+        for i in range(0, len(board)):
+            if board[i][0] != 0:
+                to_ret += 1
+            if board[i][-1] != 0:
+                to_ret += 1
+        for i in range(1, len(board) - 1):
+            if board[0][i] != 0:
+                to_ret += 1
+            if board[-1][i] != 0:
+                to_ret += 1
+        return to_ret
+
+    def get_biggest_tile_pos(self, board) -> (int, int, int):
+        max_val = 0
+        for i in range(0, len(board)):
+            for j in range(0, len(board)):
+                if board[i][j] > max_val:
+                    max_val = board[i][j]
+        for i in range(0, len(board)):
+            for j in range(0, len(board)):
+                if board[i][j] == max_val:
+                    return i, j,max_val
+        return -1, -1,0
 
 
 # part B
@@ -69,6 +187,7 @@ class MiniMaxMovePlayer(AbstractMovePlayer):
     implement get_move function according to MiniMax algorithm
     (you can add helper functions as you want).
     """
+
     def __init__(self):
         AbstractMovePlayer.__init__(self)
         # TODO: add here if needed
@@ -87,6 +206,7 @@ class MiniMaxIndexPlayer(AbstractIndexPlayer):
     implement get_indices function according to MiniMax algorithm, the value in minimax player value is only 2.
     (you can add helper functions as you want).
     """
+
     def __init__(self):
         AbstractIndexPlayer.__init__(self)
         # TODO: add here if needed
@@ -104,6 +224,7 @@ class ABMovePlayer(AbstractMovePlayer):
     implement get_move function according to Alpha Beta MiniMax algorithm
     (you can add helper functions as you want)
     """
+
     def __init__(self):
         AbstractMovePlayer.__init__(self)
         # TODO: add here if needed
@@ -121,6 +242,7 @@ class ExpectimaxMovePlayer(AbstractMovePlayer):
     implement get_move function according to Expectimax algorithm.
     (you can add helper functions as you want)
     """
+
     def __init__(self):
         AbstractMovePlayer.__init__(self)
         # TODO: add here if needed
@@ -137,6 +259,7 @@ class ExpectimaxIndexPlayer(AbstractIndexPlayer):
     implement get_indices function according to Expectimax algorithm, the value is number between {2,4}.
     (you can add helper functions as you want)
     """
+
     def __init__(self):
         AbstractIndexPlayer.__init__(self)
         # TODO: add here if needed
@@ -154,6 +277,7 @@ class ContestMovePlayer(AbstractMovePlayer):
     implement get_move function as you want to compete in the Tournament
     (you can add helper functions as you want)
     """
+
     def __init__(self):
         AbstractMovePlayer.__init__(self)
         # TODO: add here if needed
@@ -163,4 +287,3 @@ class ContestMovePlayer(AbstractMovePlayer):
         raise NotImplementedError
 
     # TODO: add here helper functions in class, if needed
-
